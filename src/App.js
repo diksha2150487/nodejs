657bdb2fbf8b452a450e95cef71898550572b964
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-08-29 00:10:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,398 @@
+import img1 from "./images/computer-img.png";
+import img2 from "./images/men-women.png";
+import img3 from "./images/map-design.svg";
+import img4 from "./images/nodejs-NODE.JS_Real-time-application.svg.png";
+import img5 from "./images/check-box1.svg";
+import img6 from "./images/angular-Node.Js_API.svg.png";
+import img7 from "./images/mvp-com.png";
+import img8 from "./images/scure.png";
+import img9 from "./images/group-part.png";
+import img10 from "./images/dark-img.png";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <div className="Nodejs_page_main">
+        <div className="Nodejs-left-cello-section">
+          <p className="plates-curve">Node.js Development Services</p>
+          <p className="colouring-text-plane">
+            As part of the Node.js community since the start, we have the right
+            expertise to make your app development dreams come true.
+          </p>
+          <button className="word-button">
+            <p> LET’S TALK</p>{" "}
+          </button>
+        </div>
+        <div className="Nodejs-right-cello">
+          <img src={img1} alt="img" className="three-circle" />
+        </div>
+      </div>
+      {/* ******************************************************************************************* */}
+      <div className="Nodejs-icon-cont-2-main">
+        <div className="js-bubble-left-part">
+          <p className="groom">Node.js Development Services</p>
+          <p className="scale-text-1">
+            We help clients from all over the world to stay ahead of the
+            competition – our team comprises seasoned Node.js developers, UI/UX
+            designers, QA specialists, and developers across other technologies
+            to help you create high-performing custom digital products for
+            companies of all sizes – from small companies to large-scale
+            enterprises..
+          </p>
+        </div>
+        <div className="js-bubble-right-part">
+          <img src={img2} id="yellow-bllok" alt="logo"></img>
+        </div>
+      </div>
+      {/* *************************************************************************************************************** */}
+      <div className="Nodejs-cont-3-290-main">
+        <p className="offring-hr">
+          <span className="js_jio">Node.js </span> Development Services We Offer
+        </p>
+        <div className="colborating-main-card">
+          <div className="card-biopo-main-1-part">
+            <div class="node-js-first-polo">
+              <div class="node-jx-Api">
+                <img src={img3} alt="logo" className="node-jsx-parrot" />
+              </div>
+              <div class="custom-jsx">
+                <div class="deploye-pretty">Custom Node.js Development</div>
+                <p className="nodes-para">
+                  When you have unique business requirements, we will help you
+                  make a custom, lightweight, cost-efficient web application
+                  with our skilled Node.js developers, tailored to your
+                  timeline, budget, and needs.
+                </p>
+              </div>
+            </div>
+            <div class="node-js-first-polo">
+              <div class="node-jx-Api">
+                <img src={img4} alt="logo" className="node-jsx-parrot" />
+              </div>
+              <div class="custom-jsx">
+                <div class="deploye-pretty">Real-time Application</div>
+                <p className="nodes-para">
+                  Real-time apps fetch data and show it to users in real-time.
+                  Due to the event-driven architecture of Node.js, it provides
+                  high speed and user- friendliness.
+                </p>
+              </div>
+            </div>
+            <div class="node-js-first-polo">
+              <div class="node-jx-Api">
+                <img src={img5} alt="logo" className="node-jsx-parrot" />
+              </div>
+              <div class="custom-jsx">
+                <div class="deploye-pretty">
+                  Enterprise Application Development
+                </div>
+                <p className="nodes-para">
+                  Our team can help you modernize and expand your enterprise
+                  capabilities by building complex, innovative, and custom-built
+                  applications.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="card-main-lefo-second">
+            <div class="node-js-first-polo">
+              <div class="node-jx-Api">
+                <img src={img6} alt="logo" className="node-jsx-parrot" />
+              </div>
+              <div class="custom-jsx">
+                <div class="deploye-pretty">
+                  API Development and Improvement
+                </div>
+                <p className="nodes-para">
+                  You can modernize your existing systems and improve their
+                  performance more effectively by integrating advanced
+                  functionalities and features with compatible APIs.
+                </p>
+              </div>
+            </div>
+            <div class="node-js-first-polo">
+              <div class="node-jx-Api">
+                <img src={img7} alt="logo" className="node-jsx-parrot" />
+              </div>
+              <div class="custom-jsx">
+                <div class="deploye-pretty">MVP development</div>
+                <p className="nodes-para">
+                  Do you have a web application idea but want to test it before
+                  investing? You can build a quick, cost-effective MVP with key
+                  features by using our Node.js web development services.
+                </p>
+              </div>
+            </div>
+            <div class="node-js-first-polo">
+              <div class="node-jx-Api">
+                <img src={img8} alt="logo" className="node-jsx-parrot" />
+              </div>
+              <div class="custom-jsx">
+                <div class="deploye-pretty">Maintenance and Support</div>
+                <p className="nodes-para">
+                  We are ready to offer a wide range of maintenance and support
+                  services like bug fixing, optimization assistance, and more to
+                  allow you to maximize your overall business.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* **************************************************************************************************************************************** */}
+      <div className="nodes-js-cont-4-main">
+        <p className="iconic-text">
+          Why<span className="queen-js">Clients Hire</span>us as a Node.js
+          Development Company
+        </p>
+        <p className="java-node-js-mix">
+          Built on top of JavaScript, Node.js is a back-end runtime environment
+          used to run web apps outside the client’s browser. It is open-source,
+          cross-platform, and used for server-side programming. It runs on the
+          Chrome V8 JavaScript engine, and was created by Ryan Dahl in 2009.
+        </p>
+        <div className="evualting-main-card-part">
+          <div class="Api-first-node-js-momument">
+            <div class="catlogo">
+              <img src={img6} alt="logo" className="deploye-line-img" />
+            </div>
+            <div class="phone-text-nodes-jsx">
+              <div class="king-pd-jsx">Qualified IT Specialists</div>
+              <p className="jsx-align-market">
+                Today there are 250+ qualified, talented, data-driven software
+                engineers working for SCAND. We offer Node.JS development
+                services and create efficient software solutions tailored to our
+                clients' business goals.
+              </p>
+            </div>
+          </div>
+          <div class="Api-first-node-js-momument">
+            <div class="catlogo">
+              <img src={img6} alt="logo" className="deploye-line-img" />
+            </div>
+            <div class="phone-text-nodes-jsx">
+              <div class="king-pd-jsx">Cost Savings</div>
+              <p className="jsx-align-market">
+                Our hourly rates are much lower than they are in Western Europe
+                and the US. Also, outsourcing software development services
+                offered by SCAND eliminate extra expenses on infrastructure,
+                office, salary, and training.
+              </p>
+            </div>
+          </div>
+          <div class="Api-first-node-js-momument">
+            <div class="catlogo">
+              <img src={img6} alt="logo" className="deploye-line-img" />
+            </div>
+            <div class="phone-text-nodes-jsx">
+              <div class="king-pd-jsx">Good Command of English</div>
+              <p className="jsx-align-market">
+                SCAND Node.JS developers are fluent in English. Although we
+                don't claim to sound like native speakers, our accent is clear
+                and easily understandable.
+              </p>
+            </div>
+          </div>
+          <div class="Api-first-node-js-momument">
+            <div class="catlogo">
+              <img src={img6} alt="logo" className="deploye-line-img" />
+            </div>
+            <div class="phone-text-nodes-jsx">
+              <div class="king-pd-jsx">1 Month Zero Billing Guarantee</div>
+              <p className="jsx-align-market">
+                If you are not satisfied with our work after the first month of
+                partnership, you do not have to pay for it.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className=" logic-button-img">
+          DISCUSS YOUR CUSTOM REQUIREMENTS
+        </div>
+      </div>
+      {/* ****************************************************************************************************************************** */}
+      <div className="node-js-contable5-main-section">
+        <p className="looft-roz">
+          What can you build with<span className="sitopo">Node js?</span>
+        </p>
+        <div className="main-jsx-sequent-hifo">
+          <div className="node-js-contst-left-part">
+            <div className="box-part-main">
+              <p className="waves-line" id="highlight-section">
+                Real-time dashboard
+              </p>
+              <p className="waves-line">Chats</p>
+              <p className="waves-line">Streaming services</p>
+              <p className="waves-line">Serverless</p>
+              <p className="waves-line">Microservices</p>
+              <p className="waves-line">Interactive applications</p>
+            </div>
+          </div>
+          <div className="node-js-contst-right-part">
+            <div className="box-pretty-walt-right">
+              <p className="top-toye">Real-time dashboard</p>
+              <p className="api-oof-para">
+                Node.js’s event-driven nature means that the charts and graphs
+                respond to the user’s input instantly and there’s no need to
+                reload the page. Node js development services are the right
+                choice when your project idea and web application relies heavily
+                on user input and output of various data types. Leading
+                providers of such solutions often turn to a Node js development
+                company that can produce efficient API development in Node for
+                web and mobile. Both web applications and mobile applications
+                developed this way prove highly efficient. And there are even
+                more Node js features for backend development with every
+                version.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ********************************************************************************** */}
+      <div className="nodes-js-cont7-perfect-main">
+        <div className="jsx-left-perfect-part">
+          <p className="letest-view">Not sure where to start?</p>
+          <p className="toggle-gap">
+            Make your next online project go smoothly with our battle-tested
+            eCommerce development services.
+          </p>
+          <button className="mata-line-circle">
+            <p> LET’S TALK</p>{" "}
+          </button>
+        </div>
+        <div className="jsx-right-perfect-part">
+          <img src={img9} alt="img" className="group-img" />
+        </div>
+      </div>
+      {/* ********************************************************************************************** */}
+      <div className="node-js-prox-8-main-part">
+        <p className="combiflame">
+          Why choose<span className="xogop">Kushel Digi</span>for your Node js
+        </p>
+        <p className="small-barry-doll"> development services?</p>
+        <div className="rigouos-test-alive">
+          <div className="risk-morning-chipo">
+            <div className="billing-mozo">
+              <div className="bogolo-ashty">
+                <hr id="short-viggly-line" />
+                01
+              </div>
+              <p className="adnting">Business-Oriented Development</p>
+              <p className="qwting-kinrij">
+                At Relevant, we deliver Node.js solutions tailored to your
+                business objectives, ensuring smooth implementation,
+                maintainability, and a significant return on investment. We stay
+                abreast of industry advancements, adhere to best practices, and
+                devise innovative, custom solutions to surpass client and
+                internal expectations.
+              </p>
+            </div>
+            <div className="billing-mozo">
+              <div className="bogolo-ashty">
+                <hr id="short-viggly-line" />
+                02
+              </div>
+              <p className="adnting">Dedicated Project Manager</p>
+              <p className="qwting-kinrij">
+                At Relevant, we deliver Node.js solutions tailored to your
+                business objectives, ensuring smooth implementation,
+                maintainability, and a significant return on investment. We stay
+                abreast of industry advancements, adhere to best practices, and
+                devise innovative, custom solutions to surpass client and
+                internal expectations.
+              </p>
+            </div>
+            <div className="billing-mozo">
+              <div className="bogolo-ashty">
+                <hr id="short-viggly-line" />
+                03
+              </div>
+              <p className="adnting">Seamless Communication</p>
+              <p className="qwting-kinrij">
+                We utilize various communication tools and channels to maintain
+                transparency and foster a strong collaborative environment,
+                making it easy for you to track progress and share feedback in
+                real-time.
+              </p>
+            </div>
+          </div>
+          <div className="rish-evening-chipoloy">
+            <div className="billing-mozo">
+              <div className="bogolo-ashty">
+                <hr id="short-viggly-line" />
+                04
+              </div>
+              <p className="adnting">Competitive Pricing</p>
+              <p className="qwting-kinrij">
+                We provide outsourced Node.js development services from Eastern
+                Europe. Benefiting from our strategic location, reduced overhead
+                expenses, and access to local expertise, we can deliver at least
+                20% more competitive rates than other Node.js outsourcing
+                alternatives.
+              </p>
+            </div>
+            <div className="billing-mozo">
+              <div className="bogolo-ashty">
+                <hr id="short-viggly-line" />
+                05
+              </div>
+              <p className="adnting">Data Security and Confidentiality</p>
+              <p className="qwting-kinrij">
+                As we prioritize confidentiality when handling our clients'
+                projects, we sign a Non-Disclosure Agreement (NDA) to safeguard
+                your ideas and information. Our team is committed to building a
+                strong collaborative relationship with you, acting as a
+                trustworthy partner throughout development.
+              </p>
+            </div>
+            <div className="billing-mozo">
+              <div className="bogolo-ashty">
+                <hr id="short-viggly-line" />
+                06
+              </div>
+              <p className="adnting">
+                Proactive Risk Assessment & Rigorous Testing
+              </p>
+              <p className="qwting-kinrij">
+                Our risk assessment procedures identify potential software
+                development challenges, while rigorous testing ensures optimal
+                functionality and uncovers bugs. These practices enable us to
+                provide secure and efficient solutions that cater to user needs.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ****************************************************************************************************************************************************** */}
+      <div className="jsx-pmyl-cont-9-main">
+        <div className="logo-img-section">
+          <img src={img10} alt="logo" id="dark-men-menu"></img>
+        </div>
+        <div className="whiting-glow-fack">
+          <p className="adpative-cloo">Got a preferred workflow? We’ll</p>
+          <p className="hickopo">adapt to your needs.</p>
+          <p className="fan-sweet">
+            We’re always flexible to fit your style of working.
+          </p>
+        </div>
+        <div className="jio-slopo-odit">LET’S TALK</div>
+      </div>
+      {/* ******************************************************************************************************************************************************************* */}
+      <div className="npde-jsx-cont6-part12">
+        <p className="solotal">
+          Discover our Node.js development service
+          <span className="ceo-joro-cwpl">solutions</span>
+        </p>
+        <p className="vijo-palota">
+          Engage with dedicated developers from 50+ countries and enhance your
+          team’s capabilities with skilled Node.js professionals. You can
+          integrate our developers into your existing team as full-time
+          contributors or entrust the end-to-end development process to our
+          multi-competent team.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
